a1b62f9c24d6ad80c01592ea742930c28fe8323b
PC-20241126RNJD\Administrator <syndred@163.com>
2026-02-08 08:52:40 +0800

    fix:twikoo http访问时谷歌浏览器拦截

--- src/config.ts
+++ src/config.ts
@@ -430,7 +430,7 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	twikoo: {
-		envId: "http://43.153.183.194:8080/",
+		envId: "https://twikoo.syndred.top/",
 		lang: SITE_LANG,
 	},
 };
